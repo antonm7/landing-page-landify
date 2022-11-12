4d539a2c3e5c4ce3e983fdbb4e82fdb4b84a2adf
Anton Migolko <migolkoanton@gmail.com>
2022-11-12 22:46:23 +0200

    design

--- src/App.tsx
+++ src/App.tsx
@@ -12,30 +12,31 @@ function App() {
   const [loading, setLoading] = useState<boolean>(true)
 
   return (
-    <div style={{overflowY:loading ? 'hidden' : 'unset'}}>
-      {loading ? 
-        <div style={{
-          zIndex:999,
-          position:'fixed',
-          paddingTop:'5rem',
-          width:'100vw',
-          height:'100%',
-          backgroundColor:'#F4EFDE',
-        }}>
-          <div className="loader">Loading...</div>
-        </div>
-        : null  
-    }
-      <div style={{opacity:loading ? 0 : 1,height:loading ? 0 : 'unset'}}>
-         <Navbar />
-         <Header loaded={() => setLoading(false)}/>
-         <Process />
-         <Story />
-         <Services />
-         <Questions />
-         <Footer />
-       </div>
-    </div>
+    <div style={{opacity:loading ? 0 : 1,height:loading ? 0 : 'unset'}}>
+    <Navbar />
+    <Header loaded={() => setLoading(false)}/>
+    <Process />
+    <Story />
+    <Services />
+    <Questions />
+    <Footer />
+  </div>
+    // <div style={{overflowY:loading ? 'hidden' : 'unset'}}>
+    //   {loading ? 
+    //     <div style={{
+    //       zIndex:999,
+    //       position:'fixed',
+    //       paddingTop:'5rem',
+    //       width:'100vw',
+    //       height:'100%',
+    //       backgroundColor:'#F4EFDE',
+    //     }}>
+    //       <div className="loader">Loading...</div>
+    //     </div>
+    //     : null  
+    // }
+     
+    // </div>
   )
 
   // return (
