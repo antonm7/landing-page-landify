f26d39c4dca94e83326d7591c73a19a262c1e1c2
Anton Migolko <migolkoanton@gmail.com>
2022-11-13 08:08:43 +0200

    style

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,35 @@ import Services from './components/Services'
 import Questions from './components/Questions'
 import Footer from './components/Footer'
 import Menu from './components/Menu'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [menu, setMenu] = useState<boolean>(false)
+  const [display,setDisplay] = useState<boolean>(false)
 
+  useEffect(() => {
+    if(menu) {
+      setTimeout(() => {
+        setDisplay(false)
+      },200)
+    } else {
+      setDisplay(true)
+    }
+    
+  },[menu])
 
   return (
     <div style={{position:'relative'}}>
       <Menu close={() => setMenu(false)} active={menu}/>
-      <Navbar openMenu={() => setMenu(true)}/>
-      <Header />
-      <Process />
-      <Story />
-      <Services />
-      <Questions />
-      <Footer />
+      <div style={{display:!display ? 'none' : 'block'}}>
+        <Navbar openMenu={() => setMenu(true)}/>
+        <Header />
+        <Process />
+        <Story />
+        <Services />
+        <Questions />
+        <Footer />
+      </div>
     </div>
   )
 }
